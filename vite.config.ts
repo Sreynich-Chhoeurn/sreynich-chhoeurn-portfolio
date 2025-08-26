411d244cd8a3eeac4c2df848e8ab73ca15b0b6f7
Sreynich-Chhoeurn <sreynich.chhoeurn@student.passerellesnumeriques.org>
2025-08-26 12:27:00 +0700

    cersion 03

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,10 @@
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import path from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(), // Allows path aliases from tsconfig.json
-  ],
-  root: '.', // project root
-  base: './', // ensures asset paths work correctly
-  publicDir: 'public', // default, can be omitted
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'src/index.html'),
-      },
-    },
-  },
-  server: {
-    port: 3000,
-    open: true,
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
 });
